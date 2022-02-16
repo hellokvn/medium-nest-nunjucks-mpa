63398f29ea8ee49c015a9017521fa7a482febf59
Kevin <hellokevinvogel@gmail.com>
2022-02-16 18:31:22 +0100

    chore(): add nunjucks +  view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  public getHello(@Res() res: Response): void {
+    const headline: string = this.appService.getHello();
+
+    return res.render('./views/index.njk', { headline });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as nunjucks from 'nunjucks';
+
+const ROOT_DIR: string = join(__dirname, '..');
+const IS_PRODUCTION: boolean = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const port: number = 3000;
+  const opts: nunjucks.ConfigureOptions = { express: app, autoescape: true, watch: !IS_PRODUCTION, noCache: !IS_PRODUCTION };
+
+  nunjucks.configure(join(ROOT_DIR, IS_PRODUCTION ? 'dist' : 'src'), opts);
+
+  app.enableCors();
+  app.set('trust proxy', 1);
+  app.set('view engine', 'njk');
+
+  await app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
 }
+
 bootstrap();
